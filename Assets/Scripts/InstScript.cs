098448f930492c1124c322b52adf0dec1fdf9ae3
Eldddir <tebenkov2222@gmail.com>
2019-07-17 19:39:46 +0400

    Добавление папок Shader и Material

---
+++ Assets/Scripts/InstScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InstScript : MonoBehaviour {
+	public GameObject  Empty1, Empty2, InstObject;
+	public Vector3 InstObjectPosition;
+	private float InstObjectScale, mousex, mousey, mousexnormal, mouseynormal;
+    private GameObject inst;
+	// Use this for initialization
+	void Start () {
+		InstObjectPosition = new Vector3((Empty1.transform.position.x + Empty2.transform.position.x) / 2, (Empty1.transform.position.y + Empty2.transform.position.y) / 2, (Empty1.transform.position.z + Empty2.transform.position.z) / 2);
+		InstObjectScale = Mathf.Sqrt(
+			(Empty1.transform.position.x - Empty2.transform.position.x) * (Empty1.transform.position.x - Empty2.transform.position.x) +
+			(Empty1.transform.position.y - Empty2.transform.position.y) * (Empty1.transform.position.y - Empty2.transform.position.y) +
+			(Empty1.transform.position.z - Empty2.transform.position.z) * (Empty1.transform.position.z - Empty2.transform.position.z)
+		);
+        inst = Instantiate(InstObject, InstObjectPosition, Quaternion.identity);
+        inst.transform.localScale = new Vector3(InstObjectScale, InstObjectScale, InstObjectScale);
+        mousexnormal = Input.mousePosition.x;
+        mouseynormal = Input.mousePosition.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InstObjectPosition = new Vector3((Empty1.transform.position.x + Empty2.transform.position.x) / 2, (Empty1.transform.position.y + Empty2.transform.position.y) / 2, (Empty1.transform.position.z + Empty2.transform.position.z) / 2);
+        InstObjectScale = Mathf.Sqrt(
+            (Empty1.transform.position.x - Empty2.transform.position.x) * (Empty1.transform.position.x - Empty2.transform.position.x) +
+            (Empty1.transform.position.y - Empty2.transform.position.y) * (Empty1.transform.position.y - Empty2.transform.position.y) +
+            (Empty1.transform.position.z - Empty2.transform.position.z) * (Empty1.transform.position.z - Empty2.transform.position.z)
+        );
+        inst.transform.position = InstObjectPosition;
+        inst.transform.localScale = new Vector3(InstObjectScale, InstObjectScale, InstObjectScale);
+        mousex = Input.mousePosition.x;
+        Empty1.transform.position = new Vector3((mousex - mousexnormal) / 15 , 0, 0);
+        mousey = Input.mousePosition.y;
+        Empty2.transform.position = new Vector3((mousey - mouseynormal) / 15, 0, 0);
+    }
+}
