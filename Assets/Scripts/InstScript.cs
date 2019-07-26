c0391eb94324c7d947add0ebccd77a81a1690743
Eldddir <tebenkov2222@gmail.com>
2019-07-26 21:36:07 +0400

    Убитая версия поворота через синусы и коссинусы

--- Assets/Scripts/InstScript.cs
+++ Assets/Scripts/InstScript.cs
@@ -2,77 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class InstScript : MonoBehaviour {
-	public GameObject  Empty1, Empty2, InstObject;
-	public Vector3 InstObjectPosition;
-	private float InstObjectScale, mousex, mousey, mousexnormal, mouseynormal;
-    private GameObject inst;
-    public float Rotx, Roty, Rotz, CosX, CosY, CosZ;
-    private float RotxP, RotyP, RotzP;
-    private Quaternion OriginalQuaternion;
+	public GameObject  Empty1, Empty2, InstObject; // объекты: 1 и 2 точки и префаб кубика
+	public Vector3 InstObjectPosition; // центральная точка, где должен быть кубик
+	private float InstObjectScale, mousex, mousey, mousexnormal, mouseynormal; // скейл, мышьХ,мышьУ и начальные точки мыши(не актуально
+    public GameObject inst; // сам кубик при инстанте сюда присваивается
+    public float Rotx, Roty, Rotz, CosX, CosY, CosZ; // углы и коссинусы поворотов
+    private float RotxP, RotyP, RotzP; // предыдущие значения углов
     // Use this for initialization
     void Start () {
+        //находится центральная точка
 		InstObjectPosition = new Vector3((Empty1.transform.position.x + Empty2.transform.position.x) / 2, (Empty1.transform.position.y + Empty2.transform.position.y) / 2, (Empty1.transform.position.z + Empty2.transform.position.z) / 2);
-		InstObjectScale = Mathf.Sqrt(
+        // находится скейл
+        InstObjectScale = Mathf.Sqrt(
 			(Empty1.transform.position.x - Empty2.transform.position.x) * (Empty1.transform.position.x - Empty2.transform.position.x) +
 			(Empty1.transform.position.y - Empty2.transform.position.y) * (Empty1.transform.position.y - Empty2.transform.position.y) +
 			(Empty1.transform.position.z - Empty2.transform.position.z) * (Empty1.transform.position.z - Empty2.transform.position.z)
 		);
+        //создаем объект
         inst = Instantiate(InstObject, InstObjectPosition, Quaternion.identity);
         inst.transform.localScale = new Vector3(InstObjectScale, InstObjectScale, InstObjectScale);
+        //для упарвления мышью
         mousexnormal = Input.mousePosition.x;
         mouseynormal = Input.mousePosition.y;
-        OriginalQuaternion = inst.transform.rotation;
-        this.transform.rotation = OriginalQuaternion;
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        // тот же самый код что в начале
         InstObjectPosition = new Vector3((Empty1.transform.position.x + Empty2.transform.position.x) / 2, (Empty1.transform.position.y + Empty2.transform.position.y) / 2, (Empty1.transform.position.z + Empty2.transform.position.z) / 2);
         InstObjectScale = Mathf.Sqrt(
             (Empty1.transform.position.x - Empty2.transform.position.x) * (Empty1.transform.position.x - Empty2.transform.position.x) +
             (Empty1.transform.position.y - Empty2.transform.position.y) * (Empty1.transform.position.y - Empty2.transform.position.y) +
             (Empty1.transform.position.z - Empty2.transform.position.z) * (Empty1.transform.position.z - Empty2.transform.position.z)
         );
-
+        // енд старт код
+        // находим коссинус Х
         CosX =
         (Empty2.transform.position.z - Empty1.transform.position.z) /
         Mathf.Sqrt(
             (Empty2.transform.position.y - Empty1.transform.position.y) * (Empty2.transform.position.y - Empty1.transform.position.y) +
             (Empty2.transform.position.z - Empty1.transform.position.z) * (Empty2.transform.position.z - Empty1.transform.position.z)
         );
-        if ((Empty2.transform.position.y - Empty1.transform.position.y) > 0) CosX = CosX * -1;
-        Rotx = Mathf.Acos(CosX) * Mathf.Rad2Deg;
+        // находим угол Х
+        Rotx =  Mathf.Asin(CosX) * Mathf.Rad2Deg;
+        // находим коссинус У
         CosY =
         (Empty2.transform.position.x - Empty1.transform.position.x) /
         Mathf.Sqrt(
             (Empty2.transform.position.x - Empty1.transform.position.x) * (Empty2.transform.position.x - Empty1.transform.position.x) +
             (Empty2.transform.position.z - Empty1.transform.position.z) * (Empty2.transform.position.z - Empty1.transform.position.z)
         );
-        Roty = Mathf.Acos(CosY) * Mathf.Rad2Deg;
-        if ((Empty2.transform.position.z - Empty1.transform.position.z) < 0) Roty = Roty * -1;
+        //Так же находим угол (с минусом стабильнее)
+        Roty = Mathf.Asin(CosY) * Mathf.Rad2Deg;
+        if ((Empty2.transform.position.z - Empty1.transform.position.z) > 0) Roty = Roty * -1;
+         //находим коссинус Z
+         CosZ =
+        (Empty2.transform.position.y - Empty1.transform.position.y) /
+        Mathf.Sqrt(
+            (Empty2.transform.position.x - Empty1.transform.position.x) * (Empty2.transform.position.x - Empty1.transform.position.x) +
+            (Empty2.transform.position.y - Empty1.transform.position.y) * (Empty2.transform.position.y - Empty1.transform.position.y)
+        );
+        //в свою очередь и угол тоже
+        Rotz = Mathf.Asin(CosZ) * Mathf.Rad2Deg;
 
-        /*if (Rotx != RotxP)
-        {
-            inst.transform.Rotate(Rotx-RotxP, 0, 0);
-            RotxP = Rotx;
-        }
-        if (Roty != RotyP)
-        {
-            inst.transform.Rotate(0, Roty - RotyP, 0);
-            RotyP = Roty;
-        }
-        if (Rotz != RotzP)
-        {
-            inst.transform.Rotate(0, 0, Rotz - RotzP);
-            RotzP = Rotz;
-        }*/
-        Quaternion RotXQuat = Quaternion.AngleAxis(Rotx, Vector3.forward);
-        Quaternion RotYQuat = Quaternion.AngleAxis(-Roty, Vector3.up);
-        //Quaternion RotZQuat = Quaternion.AngleAxis(-Rotz, Vector3.forward);
-        inst.transform.rotation = OriginalQuaternion * RotYQuat * RotXQuat;
-        this.transform.rotation = OriginalQuaternion * RotYQuat * RotXQuat;
-        inst.transform.position = InstObjectPosition;
-        inst.transform.localScale = new Vector3(InstObjectScale, InstObjectScale, InstObjectScale);
+        inst.transform.position = InstObjectPosition; // присваиваем позицию
+        inst.transform.localScale = new Vector3(InstObjectScale, InstObjectScale, InstObjectScale); // присваиваем скейл
+        inst.GetComponent<RotationScriptInst>().RotationInst(Rotx, Roty, Rotz); // изменяем угол
+        // код для упраавления мышью
         mousex = Input.mousePosition.x;
         //Empty1.transform.position = new Vector3((mousex - mousexnormal) / 15 , 0, 0);
         mousey = Input.mousePosition.y;
---
+++ Assets/Scripts/RotationScriptInst.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScriptInst : MonoBehaviour {
+    Quaternion originQuternion;
+	// Use this for initialization
+	void Start () {
+        originQuternion = transform.rotation;
+
+    }
+    public void RotationInst(float RotX,float RotY,float RotZ)
+    {
+        // преобразоывание градусов в эйлеры
+        Quaternion RotYQuat = Quaternion.AngleAxis(RotY, Vector3.up);
+        Quaternion RotXQuat = Quaternion.AngleAxis(RotX, Vector3.right);
+        Quaternion RotZQuat = Quaternion.AngleAxis(RotZ, Vector3.forward);
+        // преобразоывание градусов в эйлеры \\
+        //this.transform.rotation = originQuternion * RotXQuat * RotYQuat * RotZQuat; // использую эйлеры
+        this.transform.rotation = Quaternion.Euler(RotX, RotY, RotZ); // использую градусы
+    }
+}
